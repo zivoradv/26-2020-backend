a70b2177e69f4b1271d8ee88613b2bc3cdeae921
Zivorad <Zivorad@ZIVORAD>
2024-06-05 20:44:28 +0200

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use("/api", itemRoute)
 const PORT = process.env.PORT || 5000
 
 mongoose
-    .connect(process.env.MONGO_URI)
+    .connect("mongodb+srv://zivorad:123@cluster0.pf2r2iy.mongodb.net/levelup?retryWrites=true&w=majority")
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`) 
